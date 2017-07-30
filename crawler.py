827b8bc688b7b1157d5a640c3ab286951d43d3e0
Usurer <ivvlg@narod.ru>
2017-07-30 06:04:58 +0300

    Refactoring: following common naming convention

--- crawler.py
+++ crawler.py
@@ -17,38 +17,38 @@ import utils
 
 
 # As a result I'll have the HTML of archives page
-def GetArchivesPage(archiveUrl, index):
+def get_archives_page(archive_url, index):
     if index > 1:
-        archiveUrl = archiveUrl + '?page=' + str(index) + '&month=None'
+        archive_url = archive_url + '?page=' + str(index) + '&month=None'
 
-    print ('Getting archive page ' + archiveUrl)
-    archivePage = requests.get(archiveUrl)
+    print ('Getting archive page ' + archive_url)
+    archive_page = requests.get(archive_url)
 
-    return archivePage
+    return archive_page
 
 
 # As a result I'll have a list of photo pages' urls for photos located on the first page of Archive
-def GetLinksToPhotoPages(archivePageContent):
+def get_links_to_photo_pages(archivePageContent):
     result = []
 
-    # Addinf ? after * makes it non-greedy
+    # Adding ? after * makes it non-greedy
     # DOTALL makes a newline be matched by *
-    wrapperPattern = re.compile('(<div class="photo_info".*?</div>)', re.MULTILINE | re.DOTALL)
+    wrapper_pattern = re.compile('(<div class="photo_info".*?</div>)', re.MULTILINE | re.DOTALL)
 
     # Use findall instead of search to get all matches
-    matchResult = re.findall(wrapperPattern, archivePageContent.text)
+    match_result = re.findall(wrapper_pattern, archivePageContent.text)
 
-    if not matchResult:
+    if not match_result:
         return
 
-    linkPattern = re.compile('<a href=".*"')
+    link_pattern = re.compile('<a href=".*"')
 
-    for photoTeaserContent in matchResult:
-        photoLinkMatch = re.search(linkPattern, photoTeaserContent)
-        linkContent = photoLinkMatch.group(0)
-        hrefPattern = re.compile('".*"')
-        hrefMatchResult = re.search(hrefPattern, linkContent)
-        url = hrefMatchResult.group(0)
+    for photoTeaserContent in match_result:
+        photo_link_match = re.search(link_pattern, photoTeaserContent)
+        link_content = photo_link_match.group(0)
+        href_pattern = re.compile('".*"')
+        href_match_result = re.search(href_pattern, link_content)
+        url = href_match_result.group(0)
         url = url.strip('"/')
         result.append(url)
 
@@ -56,10 +56,10 @@ def GetLinksToPhotoPages(archivePageContent):
 
 
 # As a result I'll have an url for the photo
-def GetLinkToPhoto(photoPageUrl):
-    pageContents = requests.get(photoPageUrl).text
-    twitterRegex = re.compile('<meta property="og:image" content="(?P<url>.*?)"/>', re.MULTILINE | re.DOTALL)
-    match = re.search(twitterRegex, pageContents)
+def get_link_to_photo(photoPageUrl):
+    page_contents = requests.get(photoPageUrl).text
+    twitter_regex = re.compile('<meta property="og:image" content="(?P<url>.*?)"/>', re.MULTILINE | re.DOTALL)
+    match = re.search(twitter_regex, page_contents)
     if not match:
         return ''
 
@@ -72,60 +72,60 @@ def GetLinkToPhoto(photoPageUrl):
 
 
 # As a result I'll have photo name
-def GetPhotoName(pageContents):
-    twitterMetaRegex = re.compile('<meta name="twitter:title" content="(?P<title>.*?)">', re.MULTILINE | re.DOTALL)
-    match = re.search(twitterMetaRegex, pageContents)
+def get_photo_name(pageContents):
+    twitter_meta_regex = re.compile('<meta name="twitter:title" content="(?P<title>.*?)">', re.MULTILINE | re.DOTALL)
+    match = re.search(twitter_meta_regex, pageContents)
     if not match:
         return ''
 
     return match.groupdict()['title']
 
 
-def GetPhotoTimestamp(pageContents):
-    twitterMetaRegex = re.compile('<meta property="article:published_time" content="(?P<date>.*?)T',
+def get_photo_timestamp(pageContents):
+    twitter_meta_regex = re.compile('<meta property="article:published_time" content="(?P<date>.*?)T',
                                   re.MULTILINE | re.DOTALL)
-    match = re.search(twitterMetaRegex, pageContents)
+    match = re.search(twitter_meta_regex, pageContents)
     if not match:
         return ''
 
     return match.groupdict()['date']
 
 
-def DownloadUrlWithCaption(url, caption):
-    imagesDirectory = 'images_2'
-    utils.CreateDirectoryIfNotExists(imagesDirectory)
-    path = imagesDirectory + '/' + caption + '.jpg'
+def download_url_with_caption(url, caption):
+    images_directory = 'images_2'
+    utils.CreateDirectoryIfNotExists(images_directory)
+    path = images_directory + '/' + caption + '.jpg'
 
     if not os.path.exists(path):
-        localFile = open(path, 'wb')
-        localFile.write(requests.get(url).content)
-        localFile.close()
+        local_file = open(path, 'wb')
+        local_file.write(requests.get(url).content)
+        local_file.close()
 
 
-def DownloadPhotosFromArchivePage(archivePageIndex):
-    photographyRootUrl = "http://photography.nationalgeographic.com/"
-    archiveUrl = photographyRootUrl + "photography/photo-of-the-day/archive/"
-    archivePage = GetArchivesPage(archiveUrl, archivePageIndex)
-    photoPagesUrls = GetLinksToPhotoPages(archivePage)
+def download_photos_from_archive_page(archivePageIndex):
+    photography_root_url = "http://photography.nationalgeographic.com/"
+    archive_url = photography_root_url + "photography/photo-of-the-day/archive/"
+    archive_page = get_archives_page(archive_url, archivePageIndex)
+    photo_pages_urls = get_links_to_photo_pages(archive_page)
 
-    for url in photoPagesUrls:
-        photoUrl = GetLinkToPhoto(photographyRootUrl + url)
-        photoPageContent = requests.get(photographyRootUrl + url).text
-        photoName = GetPhotoName(photoPageContent)
-        photoName = utils.RemoveSpecialCharacters(photoName)
-        photoTimestamp = GetPhotoTimestamp(photoPageContent)
-        photoCaption = photoTimestamp + '_' + photoName
+    for url in photo_pages_urls:
+        photo_url = get_link_to_photo(photography_root_url + url)
+        photo_page_content = requests.get(photography_root_url + url).text
+        photo_name = get_photo_name(photo_page_content)
+        photo_name = utils.RemoveSpecialCharacters(photo_name)
+        photo_timestamp = get_photo_timestamp(photo_page_content)
+        photo_caption = photo_timestamp + '_' + photo_name
 
-        if len(photoUrl) > 0 and len(photoCaption) > 0:
-            print ('Downloading ' + photoCaption + ' from ' + photoUrl)
-            DownloadUrlWithCaption(photoUrl, photoCaption)
+        if len(photo_url) > 0 and len(photo_caption) > 0:
+            print ('Downloading ' + photo_caption + ' from ' + photo_url)
+            download_url_with_caption(photo_url, photo_caption)
 
     return
 
 
 def main():
     for i in range(1, 100, 1):
-        DownloadPhotosFromArchivePage(i)
+        download_photos_from_archive_page(i)
 
 
 if __name__ == '__main__':
